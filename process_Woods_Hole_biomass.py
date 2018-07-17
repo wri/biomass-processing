763673a6a479efc1b6c4a41a93ec8be1bf4d9ddf
dagibbs22 <dagibbs22@gmail.com>
2018-07-17 16:24:42 -0400

    test

--- process_Woods_Hole_biomass.py
+++ process_Woods_Hole_biomass.py
@@ -51,28 +51,25 @@ def merge_overlapping_tiles():
         print "tile1", tile1
 
 
-        for j, tile2 in enumerate(file_list[i:4]):
+        for j, tile2 in enumerate(file_list[i:len(file_list)]):
 
-            # if j > i:
+        print len(file_list)
 
-                # Extracts the tile name from the file name
-                num = len(tile2)
-                start = num - 13
-                end = num - 5
-                tile2_short = tile2[start:end]
+            # Extracts the tile name from the file name
+            num = len(tile2)
+            start = num - 13
+            end = num - 5
+            tile2_short = tile2[start:end]
 
-                print "tile2", tile2
+            print "tile2", tile2
 
-                if tile1_short == tile2_short:
+            if tile1_short == tile2_short:
 
-                    print "Overlap: tiles", tile1, "and", tile2
+                print "Overlap: tiles", tile1, "and", tile2
 
-                else:
+            else:
 
-                    print "No overlap: tiles", tile1, "and", tile2
-
-
-
+                print "No overlap: tiles", tile1, "and", tile2
 
 
 # Creates a virtual raster mosaic
