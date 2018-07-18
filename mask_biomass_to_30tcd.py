ab956a6d324da66ed2199e4430a19dc7d989ab2d
dagibbs22 <dagibbs22@gmail.com>
2018-07-18 12:16:31 -0400

    Output file names now say biomass instead of carbon

--- mask_biomass_to_30tcd.py
+++ mask_biomass_to_30tcd.py
@@ -2,37 +2,102 @@ import subprocess
 import os
 import multiprocessing
 
+def s3_to_spot(folder):
+
+    dld = ['aws', 's3', 'cp', folder, '.', '--recursive', '--exclude', '*.xml']
+    subprocess.check_call(dld)
+
+def list_tiles():
+    # Makes a text file of the tifs in the folder
+    os.system('ls *.tif > processed_biomass_tiles.txt')
+
+    total_file_list = []
+
+    # Iterates through the text file to get the names of the tiles and appends them to list
+    with open('processed_biomass_tiles.txt', 'r') as tile:
+        for line in tile:
+            # Extracts the tile name from the file name
+            num = len(line)
+            start = num - 13
+            end = num - 5
+            tile_short = line[start:end]
+
+            total_file_list.append(tile_short)
+
+    # Some tile names were in multiple ecoregions (e.g., 30N_110W in Palearctic and Nearctic). This produces only the unique tile names.
+    unique_file_list = set(total_file_list)
+
+    # The lists of unique tile names and all tile names
+    return unique_file_list, total_file_list
+
+
 def mask_biomass(tile_id):
 
     print "Hello"
 
+    biomass_tile = 's3://WHRC-carbon/WHRC_V4/Processed/{}_biomass.tif'.format(tile_id)
+    tcd_tile = 's3://gfw2-data/forest_cover/2000_treecover/Hansen_GFC2014_treecover2000_{}.tif'.format(tile_id)
+
+    calc = '--calc=A*(B>30)'
 
     print "  Masking tile..."
-    out = '{}_carbon.tif'.format(tile_id)
-    # warp = ['gdalwarp', '-t_srs', 'EPSG:4326', '-co', 'COMPRESS=LZW', '-tr', '0.00025', '0.00025', '-tap', '-te', xmin, ymin, xmax, ymax, '-dstnodata', '-9999', vrtname, out]
-    # subprocess.check_call(warp)
+    out = '{}_biomass_at_30tcd.tif'.format(tile_id)
+    cmd = ['gdal_calc.py', '-A', biomass_tile, '-B', tcd_tile,  calc, out, '--NoDataValue=-9999', '--co', 'COMPRESS=LZW']
+    subprocess.check_call(cmd)
     print "    Tile masked"
 
     print "  Copying tile to s3..."
-    s3_folder = 's3://WHRC-carbon/WHRC_V4/More_than_TCD30/'
+    s3_folder = 's3://WHRC-carbon/WHRC_V4/More_than_30tcd/'
     cmd = ['aws', 's3', 'cp', out, s3_folder]
     subprocess.check_call(cmd)
     print "    Tile copied to s3"
 
 
+# Location of the biomass tiles on s3
+s3_biomass_locn = 's3://WHRC-carbon/WHRC_V4/Processed/'
 
-# Location of the tiles on s3
-s3_locn = 's3://WHRC-carbon/WHRC_V4/Processed/'
-
-print "Checking if tiles are already downloaded..."
+print "Checking if biomass tiles are already downloaded..."
 
 if os.path.exists('./Palearctic_MapV4_60N_010W.tif') == False:
 
     # Creates a list of all the tiles on s3
-    print "  Copying raw tiles to spot machine..."
-    s3_to_spot(s3_locn)
-    print "    Raw tiles copied"
+    print "  Copying biomass tiles to spot machine..."
+    s3_to_spot(s3_biomass_locn)
+    print "    Biomass tiles copied"
 
 else:
 
-    print "  Tiles already copied"
+    print "  Biomass tiles already on machine"
+
+# Location of the tree cover density tiles on s3
+s3_tcd_locn = 's3://gfw2-data/forest_cover/2000_treecover/'
+
+print "Checking if tree cover density tiles are already downloaded..."
+
+print "Getting list of tiles..."
+unique_file_list, total_file_list = list_tiles()
+print "  Tile list retrieved. There are", len(total_file_list), "tiles total and", len(unique_file_list), "unique tiles in the dataset"
+
+
+if os.path.exists('./Hansen_GFC2014_treecover2000_50N_050W.tif') == False:
+
+    # Creates a list of all the tiles on s3
+    print "  Copying TCD tiles to spot machine..."
+    s3_to_spot(s3_tcd_locn)
+    print "    TCD tiles copied"
+
+else:
+
+    print "  TCD tiles already on machine"
+
+
+# # For a single processor
+# for tile in unique_file_list:
+#     print "Processing tile {}".format(tile)
+#     process_tile(tile)
+#     print "   Tile processed"
+
+# For multiple processors
+count = multiprocessing.cpu_count()
+pool = multiprocessing.Pool(count/3)
+pool.map(mask_biomass, unique_file_list)
--- process_Woods_Hole_biomass.py
+++ process_Woods_Hole_biomass.py
@@ -9,12 +9,12 @@ def s3_to_spot(folder):
 
 def list_tiles():
     # Makes a text file of the tifs in the folder
-    os.system('ls *.tif > spot_carbon_tiles.txt')
+    os.system('ls *.tif > spot_biomass_tiles.txt')
 
     total_file_list = []
 
     # Iterates through the text file to get the names of the tiles and appends them to list
-    with open('spot_carbon_tiles.txt', 'r') as tile:
+    with open('spot_biomass_tiles.txt', 'r') as tile:
         for line in tile:
             # Extracts the tile name from the file name
             num = len(line)
@@ -30,49 +30,10 @@ def list_tiles():
     # The lists of unique tile names and all tile names
     return unique_file_list, total_file_list
 
-def merge_overlapping_tiles():
-
-    file_list = []
-
-    with open('spot_carbon_tiles.txt', 'r') as tile:
-        for line in tile:
-
-            file_list.append(line)
-
-    # for i, tile1 in enumerate(file_list):
-    for i, tile1 in enumerate(file_list):
-        # Extracts the tile name from the file name
-        num = len(tile1)
-        start = num - 13
-        end = num - 5
-        tile1_short = tile1[start:end]
-
-        print "tile1", tile1
-
-        for j, tile2 in enumerate(file_list[i+1:len(file_list)]):
-
-            # Extracts the tile name from the file name
-            num = len(tile2)
-            start = num - 13
-            end = num - 5
-            tile2_short = tile2[start:end]
-
-            print "tile2", tile2
-
-            if tile1_short == tile2_short:
-
-                print "Overlap: tiles", tile1, "and", tile2
-                merge = []
-
-            else:
-
-                print "No overlap: tiles", tile1, "and", tile2
-
-
 # Creates a virtual raster mosaic
 def create_vrt():
 
-    vrtname = 'carbon_v4.vrt'
+    vrtname = 'biomass_v4.vrt'
     os.system('gdalbuildvrt {0} *.tif'.format(vrtname))
 
     return vrtname
@@ -105,7 +66,7 @@ def process_tile(tile_id):
     print "    Coordinates are: ymax-", ymax, "; ymin-", ymin, "; xmax-", xmax, "; xmin-", xmin
 
     print "  Warping tile..."
-    out = '{}_carbon.tif'.format(tile_id)
+    out = '{}_biomass.tif'.format(tile_id)
     warp = ['gdalwarp', '-t_srs', 'EPSG:4326', '-co', 'COMPRESS=LZW', '-tr', '0.00025', '0.00025', '-tap', '-te', xmin, ymin, xmax, ymax, '-dstnodata', '-9999', vrtname, out]
     subprocess.check_call(warp)
     print "    Tile warped"
@@ -139,16 +100,12 @@ print "Getting list of tiles..."
 unique_file_list, total_file_list = list_tiles()
 print "  Tile list retrieved. There are", len(total_file_list), "tiles total and", len(unique_file_list), "unique tiles in the dataset"
 
-# print "Checking for tiles to merge (tiles that are in multiple ecoregions)"
-# merge_overlapping_tiles()
-# print "  Tiles merged"
-
 print "Creating vrt..."
 vrtname = create_vrt()
 print "  vrt created"
 
 # # For a single processor
-# for tile in file_list:
+# for tile in unique_file_list:
 #     print "Processing tile {}".format(tile)
 #     process_tile(tile)
 #     print "   Tile processed"
