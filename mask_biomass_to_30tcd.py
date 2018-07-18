4dc5b14dad5356e3cf20ce5fac16512f51bbe35b
dagibbs22 <dagibbs22@gmail.com>
2018-07-18 14:52:59 -0400

    Working on masking script now

--- mask_biomass_to_30tcd.py
+++ mask_biomass_to_30tcd.py
@@ -4,14 +4,14 @@ import multiprocessing
 
 def s3_to_spot(folder):
 
-    dld = ['aws', 's3', 'cp', folder, '.', '--recursive', '--exclude', '*.xml']
+    dld = ['aws', 's3', 'cp', folder, '.', '--recursive']
     subprocess.check_call(dld)
 
 def list_tiles():
     # Makes a text file of the tifs in the folder
-    os.system('ls *.tif > processed_biomass_tiles.txt')
+    os.system('ls *biomass.tif > processed_biomass_tiles.txt')
 
-    total_file_list = []
+    biomass_file_list = []
 
     # Iterates through the text file to get the names of the tiles and appends them to list
     with open('processed_biomass_tiles.txt', 'r') as tile:
@@ -22,32 +22,29 @@ def list_tiles():
             end = num - 5
             tile_short = line[start:end]
 
-            total_file_list.append(tile_short)
-
-    # Some tile names were in multiple ecoregions (e.g., 30N_110W in Palearctic and Nearctic). This produces only the unique tile names.
-    unique_file_list = set(total_file_list)
+            biomass_file_list.append(tile_short)
 
     # The lists of unique tile names and all tile names
-    return unique_file_list, total_file_list
+    return biomass_file_list
 
 
 def mask_biomass(tile_id):
 
-    print "Hello"
+    tcd_mask = 30
 
     biomass_tile = 's3://WHRC-carbon/WHRC_V4/Processed/{}_biomass.tif'.format(tile_id)
     tcd_tile = 's3://gfw2-data/forest_cover/2000_treecover/Hansen_GFC2014_treecover2000_{}.tif'.format(tile_id)
 
-    calc = '--calc=A*(B>30)'
+    calc = '--calc=A*(B>{})'.format(tcd_mask)
 
-    print "  Masking tile..."
-    out = '{}_biomass_at_30tcd.tif'.format(tile_id)
+    print "  Masking tile", tile_id, "..."
+    out = '{0}_biomass_at_{1}tcd.tif'.format(tile_id, tcd_mask)
     cmd = ['gdal_calc.py', '-A', biomass_tile, '-B', tcd_tile,  calc, out, '--NoDataValue=-9999', '--co', 'COMPRESS=LZW']
     subprocess.check_call(cmd)
     print "    Tile masked"
 
     print "  Copying tile to s3..."
-    s3_folder = 's3://WHRC-carbon/WHRC_V4/More_than_30tcd/'
+    s3_folder = 's3://WHRC-carbon/WHRC_V4/More_than_{}tcd/'.format(tcd_mask)
     cmd = ['aws', 's3', 'cp', out, s3_folder]
     subprocess.check_call(cmd)
     print "    Tile copied to s3"
@@ -70,8 +67,8 @@ else:
     print "  Biomass tiles already on machine"
 
 print "Getting list of biomass tiles..."
-unique_file_list, total_file_list = list_tiles()
-print "  Tile list retrieved. There are", len(total_file_list), "tiles total and", len(unique_file_list), "unique tiles in the dataset"
+file_list = list_tiles()
+print "  Biomass tile list retrieved. There are", len(file_list), "biomass tiles total."
 
 # Location of the tree cover density tiles on s3
 s3_tcd_locn = 's3://gfw2-data/forest_cover/2000_treecover/'
